0000becab86914637d96f98052f79f83495967fa
Joaquin Rodriguez <rjoaquin@microsoft.com>
2023-05-08 22:31:20 -0500

    capz fixes

--- capz-init.sh
+++ capz-init.sh
@@ -1,41 +1,19 @@
 #!/bin/bash
-# Enable support for managed topologies and experimental features
-export CLUSTER_TOPOLOGY=true
+
 export EXP_MACHINE_POOL=true
-
-# Create an Azure Service Principal in the Azure portal. (Note: Make sure this Service Principal has access to the resource group)
-# Create an Azure Service Principal
-export AZURE_SP_NAME="kubecon23capi$RANDOM"
-
-export AZURE_SUBSCRIPTION_ID=$(az account show --query id -o tsv)
-export AZURE_TENANT_ID=$(az account show --query tenantId -o tsv)
-export AZURE_CLIENT_SECRET=$(az ad sp create-for-rbac --name $AZURE_SP_NAME --role contributor --scopes="/subscriptions/${AZURE_SUBSCRIPTION_ID}" --query password -o tsv)
-export AZURE_CLIENT_ID=$(az ad sp list --display-name $AZURE_SP_NAME --query "[0].appId" -o tsv)
-
-az role assignment create \
---role "Contributor" --assignee $AZURE_CLIENT_ID --scope $AZURE_SUBSCRIPTION_ID
-
-# Base64 encode the variables
-export AZURE_SUBSCRIPTION_ID_B64="$(echo -n "$AZURE_SUBSCRIPTION_ID" | base64 | tr -d '\n')"
-export AZURE_TENANT_ID_B64="$(echo -n "$AZURE_TENANT_ID" | base64 | tr -d '\n')"
-export AZURE_CLIENT_ID_B64="$(echo -n "$AZURE_CLIENT_ID" | base64 | tr -d '\n')"
-export AZURE_CLIENT_SECRET_B64="$(echo -n "$AZURE_CLIENT_SECRET" | base64 | tr -d '\n')"
-
-# Settings needed for AzureClusterIdentity used by the AzureCluster
 export AZURE_CLUSTER_IDENTITY_SECRET_NAME="cluster-identity-secret"
-export CLUSTER_IDENTITY_NAME="cluster-identity"
-export AZURE_CLUSTER_IDENTITY_SECRET_NAMESPACE=$1
+export AZURE_SUBSCRIPTION_ID=$(az account show --query id -o tsv)
+export AZURE_CLUSTER_IDENTITY_SECRET_NAMESPACE="default"
 
-# Create a secret to include the password of the Service Principal identity created in Azure
-# This secret will be referenced by the AzureClusterIdentity used by the AzureCluster
-kubectl create secret generic "${AZURE_CLUSTER_IDENTITY_SECRET_NAME}" \
---from-literal=clientSecret="${AZURE_CLIENT_SECRET}" \
---from-literal=clientId="${AZURE_CLIENT_ID}" \
--n ${AZURE_CLUSTER_IDENTITY_SECRET_NAMESPACE}
+export AZURE_CAPZ_SP=$(az ad sp create-for-rbac --role Contributor --name capzSP --scopes="/subscriptions/${AZURE_SUBSCRIPTION_ID}" --sdk-auth)
+export AZURE_CLIENT_SECRET="$(echo $AZURE_CAPZ_SP | jq -r .clientSecret | tr -d '\n')"
+export AZURE_CLIENT_ID="$(echo $AZURE_CAPZ_SP | jq -r .clientId | tr -d '\n')"
+export AZURE_TENANT_ID="$(echo $AZURE_CAPZ_SP | jq -r .tenantId | tr -d '\n')"
 
-# Initialize the management cluster for azure
-clusterctl init --bootstrap "-" --control-plane "-" --infrastructure azure
+kubectl create secret generic "${AZURE_CLUSTER_IDENTITY_SECRET_NAME}" --from-literal=clientSecret="${AZURE_CLIENT_SECRET}"
 
-#sleep 10
+clusterctl init --infrastructure azure
+
+sleep 30
 # Create and apply an AzureClusterIdentity
-#envsubst < manifests/templates/aks-cluster-identity.yaml | kubectl apply -f -
+envsubst < manifests/templates/aks-cluster-identity.yaml | kubectl apply -f -
